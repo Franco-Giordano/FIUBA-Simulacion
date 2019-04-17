ed38e503b8da2009786cd4f0ebb048eaa726e1c8
Franco-Giordano <francogior98@gmail.com>
2019-04-17 20:26:11 -0300

    Ya arreglado la normal, ej4

--- ejsTP1_4.py
+++ ejsTP1_4.py
@@ -3,18 +3,17 @@ import math
 from matplotlib import pyplot as plt
 
 
-#corregir: intentar generar una normal estandar y luego correrla
 
-c = 4.365756303e6 #haciendo fX(t)/fY(t) se halla que este valor de t maximiza el cociente
+c = 1.3 #haciendo fX(t)/fY(t) se halla que este valor (aproximado) de t maximiza el cociente
 
-realizaciones = []
+realizaciones_std = []
 
 def cociente(t):
-	return (1/math.sqrt(2*math.pi*36)) * math.exp(t-40 -((t-40)**2 / 72)) * 1/c
+	return (1/math.sqrt(2*math.pi)) * math.exp(t - (t**2 / 2)) * 1/c
 
 for i in range(100000):
 
-	t = random.expovariate(1) + 40 #la corro 40 asi la centro igual que la normal que quiero generar
+	t = random.expovariate(1) #la corro 40 asi la centro igual que la normal que quiero generar
 
 	u = random.random()
 
@@ -24,10 +23,13 @@ for i in range(100000):
 		u2 = random.random()
 
 		if u2 < 0.5:
-			realizaciones.append(t)
+			realizaciones_std.append(t)
 
 		else:
-			realizaciones.append(-t)
-print(realizaciones)
-plt.hist(realizaciones)
+			realizaciones_std.append(-t)
+
+#hago el cambio de variable para que tenga una normal con media 40,desvio 6
+realizaciones = [z*6 + 40 for z in realizaciones_std]
+
+plt.hist(realizaciones,100)
 plt.show()
