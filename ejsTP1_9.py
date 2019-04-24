11248b3e6ab02d088c384d9784528d3a44739623
Franco-Giordano <francogior98@gmail.com>
2019-04-24 11:37:15 -0300

    Limpiar

--- ejsTP1_9.py
+++ ejsTP1_9.py
@@ -1,11 +1,13 @@
 from ejsTP1_1 import realizacionesEstandarGCL
 
 from matplotlib import pyplot as plt
-import math
 
-#calculo pn hasta que se queda igual
+from scipy import stats as sp
+
+
 a,b = 0.2,0.5
 
+#calculo las probabilidades, corto cuando ya no distingo diferencia
 probs = [b-a]
 termino = False
 i = 1
@@ -20,6 +22,34 @@ while not termino:
 		i += 1
 
 
-print(probs)
 
 muestras = realizacionesEstandarGCL()
+
+#calculo cantidad de veces que cai 0 afuera, 1 afuera, 2 afuera...
+primero = True
+contador_local = 0
+numeros = {}
+for x in muestras:
+	if (x>=0.2 and x<=0.5 and primero):
+		primero = False
+	if (x>=0.2 and x<=0.5 and not primero):
+		veces = numeros.get(contador_local,0)
+		numeros[contador_local] = veces + 1
+		contador_local = 0
+	if ((x<0.2 or x>0.5) and not primero):
+		contador_local+=1
+
+
+#ajusto para que sean frecuencias
+total = sum(numeros.values())
+for k in numeros.keys():
+	numeros[k] /= total
+
+#lo paso a lista y relleno con 0 donde no tuve apariciones
+lista = []
+for i in range(2084):
+	lista.append(numeros.get(i,0))
+
+print(probs, lista)
+
+print(sp.chisquare(lista, f_exp=probs))
